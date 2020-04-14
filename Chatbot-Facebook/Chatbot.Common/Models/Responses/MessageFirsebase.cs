d083a8e561456ca9c11f3006e865768c27bf9423
tranquang229 <tranquang229@gmail.com>
2020-04-14 22:15:20 +0700

    Create

---
+++ Chatbot-Facebook/Chatbot.Common/Models/Responses/MessageFirsebase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatbot.Common.Models.Responses
+{
+    public class MessageFirsebase
+    {
+        public string Id { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Content { get; set; }
+        public DateTime? TimeReceived { get; set; }
+
+        public DateTime? BlockUntil { get; set; }
+        public bool? BlockAll { get; set; }
+    }
+}
---
+++ Chatbot-Facebook/Chatbot.ConsoleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatbot.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Basic_Usage_Custom.Run().GetAwaiter().GetResult();
+        }
+    }
+}
